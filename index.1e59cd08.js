289fb6048dec57c735132ed904215773cf52f114
Oleksandr26 <103263665+Oleksandr26@users.noreply.github.com>
2022-06-08 21:31:33 +0000

    Deploying to gh-pages from @ Oleksandr26/hellen-project@a6da5c1f6796f90eda5e0745eaf2809b837bd91b 🚀

--- index.1e59cd08.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuModalBtn:document.querySelector("[data-menu-modal-open]"),modal:document.querySelector("[data-modal]")};e.openMenuModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
-//# sourceMappingURL=index.1e59cd08.js.map
